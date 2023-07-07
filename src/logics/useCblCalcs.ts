37e8b5194eb21cecebc447b0b803322114725bc8
Vadim Fattakhov <vadfthv@gmail.com>
2023-07-07 14:05:43 -0500

    Added ppt5ft to useCblCalcs

--- src/logics/useCblCalcs.ts
+++ src/logics/useCblCalcs.ts
@@ -8,28 +8,52 @@ interface CBLTool {
 interface CBLTools {
   1.69: CBLTool;
   2.75: CBLTool;
+  3.375: CBLTool;
   4.5: CBLTool;
 }
 // find out the id of 4.5 tool
-const cblTools: CBLTools = {
-  1.69: { od: 1.6875, id: 1.5 },
-  2.75: { od: 2.75, id: 2 },
+// or play with tools id to match table Travel Time
+export const cblTools: CBLTools = {
+  1.69: { od: 1.6875, id: 0.89 },
+  2.75: { od: 2.75, id: 1.45 },
+  3.375: { od: 3.375, id: 1.8 },
   4.5: { od: 4.5, id: 4 },
 };
 
-export const useCblCalcs = (csg: PipeSpecs, toolOd: keyof CBLTools): number => {
+export const useCblCalcs = (
+  csg: PipeSpecs,
+  toolOd: keyof CBLTools
+): { ppt3ft: number; ppt5ft: number } => {
   const dTsilicone = 226;
-  const dTfrWater = 203;
+  const dTfrWater = 205.9;
+  const dTcsg = 57;
 
   const { od, id } = cblTools[toolOd];
 
-  const dTtool: number = (dTsilicone * ((od - id) / 2)) / 12;
-  console.log(dTtool);
+  const ttTool: number = (dTsilicone * ((od - id) / 2)) / 12;
 
-  // const dTcsg;
+  // create validation: if csg id is smaller than (tool OD + certain value),
 
-  // const dTfluid;
+  const normalDistance = ((csg.id - cblTools[toolOd].od) * 0.5) / 12;
+  // FLUID calcs
+  const asin = Math.asin(dTcsg / dTfrWater);
+  const angle = Math.atan(asin);
+  // const degrees = (angle * 180) / Math.PI;
+  const fluidPath = normalDistance / Math.cos(angle);
+  const ttFluid = fluidPath * dTfrWater;
 
-  // const ppt = 2 * dTtool + dTcsg + 2 * dTfluid;
-  return 1;
+  // CASING calcs
+  const csgReduction = normalDistance * Math.tan(angle);
+
+  const csg3ftPath = 3 - csgReduction * 2;
+  const ttCsg3ft = dTcsg * csg3ftPath;
+  const ppt3ft = 2 * ttTool + ttCsg3ft + 2 * ttFluid;
+
+  const csg5ftPath = 5 - csgReduction * 2;
+  const ttCsg5ft = dTcsg * csg5ftPath;
+  const ppt5ft = 2 * ttTool + ttCsg5ft + 2 * ttFluid;
+  console.log('ppt3ft: ', ppt3ft);
+  console.log('ppt5ft: ', ppt5ft);
+
+  return { ppt3ft, ppt5ft };
 };
--- src/pages/CBL.tsx
+++ src/pages/CBL.tsx
@@ -1,15 +1,23 @@
 import { NavHeader, PipeSelector, PipeSpecsDisplay } from '../components';
 import { casingData } from '../database/casingsTubings';
 import { useCblCalcs } from '../logics/useCblCalcs';
+import { useSelector } from 'react-redux';
+import { StoreState } from '../store';
 
 export const CBL = () => {
-  useCblCalcs(casingData[0], 1.69);
+  const { casing } = useSelector((store: StoreState) => store.cbl);
+  console.clear();
+  if (casing) {
+    useCblCalcs(casing, 1.69);
+  }
 
   return (
     <>
       <NavHeader>Cement Bond Log Calcs</NavHeader>
       <PipeSelector pipeData={casingData} typeId="casing" />
-      <PipeSpecsDisplay typeId="casing" specs={['od', 'id']} />
+      {/* add tool selector */}
+      {/* add fluid  selector */}
+      <PipeSpecsDisplay typeId="casing" specs={['od', 'id']} pipeThck />
     </>
   );
 };
