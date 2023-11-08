69445c5d515c7dd820cadc4b3fdac4e081a6d393
Vadim Fattakhov <vadfthv@gmail.com>
2023-11-07 21:16:40 -0600

    Fixed revertToMetric decimals

--- src/logics/useConvertUnits.ts
+++ src/logics/useConvertUnits.ts
@@ -87,23 +87,23 @@ export const useConvertUnits = () => {
   function revertToMetric(input: number, unitType: UnitType): number {
     switch (unitType) {
       case DepthUnits.FT:
-        return +(input *= 0.3048).toFixed(0);
+        return +(input *= 0.3048);
       case WeightUnits.LBS:
-        return +(input *= 0.45).toFixed(0);
+        return +(input *= 0.45);
       case DiameterUnits.INCH:
-        return +(input *= 25.4).toFixed(2);
+        return +(input *= 25.4);
       case PressureUnits.PSI:
-        return +(input /= 14.7).toFixed(0);
+        return +(input /= 14.7);
       case CapacityUnits.BBL:
-        return +(input *= 0.158987294928).toFixed(2);
+        return +(input *= 0.158987294928);
       case TempUnits.DEGF:
-        return +((5 * (input - 32)) / 9).toFixed(2);
+        return +((5 * (input - 32)) / 9);
       case ResistivityUnits.OHM_KFT:
-        return +(input /= 0.3048).toFixed(2);
+        return +(input /= 0.3048);
       case PumpRateUnits.BBLMIN:
-        return +(input *= 0.1589873).toFixed(1);
+        return +(input *= 0.1589873);
       case VelocityUnits.FTMIN:
-        return +(input *= 0.3048).toFixed(0);
+        return +(input *= 0.3048);
       default:
         return input;
     }
