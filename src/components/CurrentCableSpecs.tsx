1f90e2fed5c33e39bbbd21845760ace9ca127629
Vadim Fattakhov <vadfthv@gmail.com>
2024-01-04 22:06:34 -0600

    Additional cable specs

--- src/components/CurrentCableSpecs.tsx
+++ src/components/CurrentCableSpecs.tsx
@@ -9,6 +9,7 @@ interface CurrentCableSpecsProps {
 }
 
 // additional cable specs
+// additional cable specs
 
 export const CurrentCableSpecs: FC<CurrentCableSpecsProps> = ({ specs }) => {
   const { weightUnits, depthUnits, diameterUnits, resistivityUnits } =
