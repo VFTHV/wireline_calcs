47cc4a5cfd04f31b9fca32b1d20c6920524e8af3
Vadim Fattakhov <vadfthv@gmail.com>
2024-01-13 20:50:16 -0700

    Resize logics

--- src/components/LargeScreenWrapper.tsx
+++ src/components/LargeScreenWrapper.tsx
@@ -5,6 +5,7 @@ interface LargeScreenWrapperProps {
   children: ReactNode;
 }
 // resize event listener logics
+// resize event listener logics
 
 export const LargeScreenWrapper: FC<LargeScreenWrapperProps> = ({
   children,
