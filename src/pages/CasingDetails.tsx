2af0dd66ca45f644744668ce2d42eeee70ec29c3
Vadim Fattakhov <vadfthv@gmail.com>
2024-01-14 21:34:38 -0700

    New casing details

--- src/pages/CasingDetails.tsx
+++ src/pages/CasingDetails.tsx
@@ -4,11 +4,6 @@ import { NavHeader, PipeSelector, PipeSpecsDisplay } from '../components';
 import { LargeScreenWrapper } from '../components/LargeScreenWrapper';
 
 export const CasingDetails: FC = () => {
-  // New casing details
-  // New casing details
-  // New casing details
-  // New casing details
-  // New casing details
   return (
     <LargeScreenWrapper>
       <NavHeader>Casing/Tubing Specs</NavHeader>
