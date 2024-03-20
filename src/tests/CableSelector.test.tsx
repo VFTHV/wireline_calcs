1a922a863380f5f5c4ab399fc2df10b3f5c6ae4b
Vadim Fattakhov <vadfthv@gmail.com>
2024-03-19 21:04:03 -0500

    installed vitest

---
+++ src/tests/CableSelector.test.tsx
@@ -0,0 +1,8 @@
+import { CableSelector } from '../components';
+import { render, screen } from '@testing-library/react';
+
+// it('renders cable selector', () => {
+//   render(<CableSelector />)
+//   const selectElement = screen.
+//   expect(selectElement).
+// })
