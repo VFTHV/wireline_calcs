50dcc3e596d95b684270d99997ca17f7667c6ebc
Vadim Fattakhov <vadfthv@gmail.com>
2024-04-13 16:47:10 -0500

    removed __test__ folder

--- src/components/__test__/NavHeader.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { NavHeader } from '..';
-
-it('renders text strings passed as children', async () => {
-  render(<NavHeader>My Header</NavHeader>);
-  const headingElement = screen.getByText(/my header/i);
-  expect(headingElement).toContain(/my header/i);
-});
