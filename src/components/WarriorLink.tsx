7bb1ddb8f3983bc1b041e132c820af638d00ff4d
Vadim Fattakhov <vadfthv@gmail.com>
2024-04-13 17:13:40 -0500

    coral link to course

--- src/components/WarriorLink.tsx
+++ src/components/WarriorLink.tsx
@@ -8,7 +8,7 @@ interface WarriorLinkProps {
 
 export const WarriorLink: FC<WarriorLinkProps> = ({ to }) => {
   return (
-    <li className="navitem">
+    <li className="navitem coral">
       <IoMdBook />
       <a href={to}>New: Warrior Logging Course!!!</a>
       <GoChevronRight />
