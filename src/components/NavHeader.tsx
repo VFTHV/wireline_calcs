0061ae89916805805aaffe00d3d493d1029f49a2
Vadim Fattakhov <vadfthv@gmail.com>
2024-05-31 21:48:20 -0500

    navheaderprops

--- src/components/NavHeader.tsx
+++ src/components/NavHeader.tsx
@@ -11,6 +11,7 @@ interface NavHeaderProps {
 // NavHeaderProps
 // NavHeaderProps
 // NavHeaderProps
+// NavHeaderProps
 
 export const NavHeader: FC<NavHeaderProps> = ({ children }) => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
