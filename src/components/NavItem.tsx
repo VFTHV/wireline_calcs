70ef00b1876f790e408d6bbb352be8e1d6c10b88
Vadim Fattakhov <vadfthv@gmail.com>
2024-06-02 09:35:46 -0500

    admin nav

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -11,35 +11,12 @@ interface NavItemProps {
 
 export const NavItem: FC<NavItemProps> = ({ to, children, icon }) => {
   return (
-    <>
-      <NavLink aria-label={`link item to ${to}`} to={to}>
-        <li className="navitem">
-          {icon ? icon : <BsCalculator />}
-          {children}
-          <GoChevronRight />
-        </li>
-      </NavLink>
-      <NavLink aria-label={`link item to ${to}`} to={to}>
-        <li className="navitem">
-          {icon ? icon : <BsCalculator />}
-          {children}
-          <GoChevronRight />
-        </li>
-      </NavLink>
-      <NavLink aria-label={`link item to ${to}`} to={to}>
-        <li className="navitem">
-          {icon ? icon : <BsCalculator />}
-          {children}
-          <GoChevronRight />
-        </li>
-      </NavLink>
-      <NavLink aria-label={`link item to ${to}`} to={to}>
-        <li className="navitem">
-          {icon ? icon : <BsCalculator />}
-          {children}
-          <GoChevronRight />
-        </li>
-      </NavLink>
-    </>
+    <NavLink aria-label={`link item to ${to}`} to={to}>
+      <li className="navitem">
+        {icon ? icon : <BsCalculator />}
+        {children}
+        <GoChevronRight />
+      </li>
+    </NavLink>
   );
 };
