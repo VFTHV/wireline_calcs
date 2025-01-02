7078ddabfa7daee66ddeba68447d8bc4437ba5a5
Vadim Fattakhov <vadfthv@gmail.com>
2025-01-01 17:01:14 -0700

    refactor: remove unnecessary dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { BrowserRouter } from 'react-router-dom';
 import { store } from './store';
 import { Provider } from 'react-redux';
 import ShadowDomWrapper from './components/ShadowDomWrapper.tsx';
@@ -11,11 +10,9 @@ ReactDOM.createRoot(
 ).render(
   <ShadowDomWrapper>
     <Provider store={store}>
-      <BrowserRouter>
-        <React.StrictMode>
-          <App />
-        </React.StrictMode>
-      </BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
     </Provider>
   </ShadowDomWrapper>
 );
